8a347a90157744ec2f779e63404cc102dcd01365
Riddhub <unkownrid@gmail.com>
2023-08-12 13:34:32 +0300

    - add WTFForm

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
+
+
+# FORMS
+class NamerForm(FlaskForm):
+    name = StringField("What's your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route('/')
@@ -11,6 +21,16 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html', name=name, form=form)
 
 
 @app.errorhandler(404)
