d03503ac22064c0604022fbcb189e70409d57151
Riddhub <unkownrid@gmail.com>
2023-08-13 12:30:15 +0300

    - add SQLALCHEMY

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
+
+db = SQLAlchemy(app)
+
+
+# MODELS
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 # FORMS
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
 class NamerForm(FlaskForm):
     name = StringField("What's your Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
@@ -30,12 +52,35 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash(f'Flash message {name}', category='message')
     return render_template('name.html', name=name, form=form)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    # email = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash('User added successfully!')
+    our_users = Users.query.order_by(Users.created)
+    return render_template('add_user.html',
+                           form=form,
+                           name=name,
+                           our_users=our_users,)
 
 
 if __name__ == '__main__':
