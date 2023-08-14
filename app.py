108e9f306f69a7360a4e8ea75902a0d70ddc9b59
Riddhub <unkownrid@gmail.com>
2023-08-14 14:14:56 +0300

    - add update and delete function

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from datetime import datetime
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 # MODELS
@@ -17,6 +19,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False, unique=True)
+    favorite_color = db.Column(db.String(120), default='Red')
     created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __str__(self):
@@ -27,6 +30,7 @@ class Users(db.Model):
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
+    favorite_color = StringField("Favorite Color")
     submit = SubmitField("Submit")
 
 
@@ -69,18 +73,62 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data)
+            user = Users(name=form.name.data,
+                         email=form.email.data,
+                         favorite_color=form.favorite_color.data)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
+        form.favorite_color.data = ''
         flash('User added successfully!')
     our_users = Users.query.order_by(Users.created)
     return render_template('add_user.html',
                            form=form,
                            name=name,
                            our_users=our_users,)
+
+
+@app.route('/update_record/<int:id>', methods=['POST', 'GET'])
+def update_record(id):
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == 'POST':
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        name_to_update.favorite_color = request.form['favorite_color']
+        try:
+            db.session.commit()
+            flash('User Updated Successfully!')
+            return render_template('update_record.html',
+                                   form=form,
+                                   name_to_update=name_to_update,)
+        except:
+            flash('Error Updating!')
+            return render_template('update_record.html',
+                                   form=form,
+                                   name_to_update=name_to_update, )
+
+    else:
+        return render_template('update_record.html',
+                               form=form,
+                               name_to_update=name_to_update,)
+
+
+@app.route('/delete_record/<int:id>')
+def delete_record(id):
+    user_to_delete = Users.query.get_or_404(id)
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash('User Deleted!')
+        return redirect(url_for('add_user'))
+    except:
+        flash('Deleting Error')
+        return redirect(url_for('add_user'))
+
+
 
 
 if __name__ == '__main__':
